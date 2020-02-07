6b82a44bec6d2b51cf2c9e3d25c6416f69aeb243
Cibirkaite <cibirkai@egb080000037.lancs.local>
2020-02-07 09:34:17 +0000

    [it's a play on creating a poem]

---
+++ PoemOnTheOtherHand.java
@@ -0,0 +1,10 @@
+public class PoemOnTheOtherHand
+{
+    public static void main (String [] args)
+    {
+        //This is the try to do
+        //Something that I have no clue
+        //How it works, so I am blue
+        //But I will manage it is cool
+    }
+}
