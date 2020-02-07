bc10ceb4c8e93e495202ce5cdd270802b415fce0
Cibirkaite <cibirkai@egb080000037.lancs.local>
2020-02-07 10:11:22 +0000

    this is the final few lines on my personal 'master piece'

--- PoemOnTheOtherHand.java
+++ PoemOnTheOtherHand.java
@@ -2,14 +2,18 @@ public class PoemOnTheOtherHand
 {
     public static void main (String [] args)
     {
-        //This is the try to do
+        //This is the attempt to do
         //Something that I have no clue
         //How it works, so I am blue
-        //But I will manage it is cool
+        //But I will manage - it is cool
 
         //I did the first few lines - alright
         //The Git Gods accepted - this is fine
         //I will try repeating this again
         //It's worth checking I took this up ok
+
+        //Seems like this is going breezy
+        //That's the end, folks, we got this! EASY!
+
     }
 }
